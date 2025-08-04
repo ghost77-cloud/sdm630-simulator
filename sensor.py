7446634420e0d1f17f10231e2402b719c4394395
Mirko Golze <mirko.golze@gmail.com>
2025-08-04 21:58:57 +0200

    start modbus server at integration startup

--- sensor.py
+++ sensor.py
@@ -1,36 +1,85 @@
 import logging
+import asyncio
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.const import CONF_NAME, ENERGY_WATT_HOUR, POWER_WATT
 from homeassistant.helpers.discovery import async_load_platform
+from pymodbus.server import StartAsyncTcpServer
+from modbus_server import (
+    context,
+    identity,
+    input_reg_manager,
+)
+from sdm630_input_registers import TOTAL_POWER
 
 _LOGGER = logging.getLogger(__name__)
 
 DEFAULT_NAME = "SDM630 Simulated Meter"
+SCAN_INTERVAL = 10  # Update every 10 seconds
+
+async def start_modbus_server():
+    """Start the Modbus server asynchronously."""
+    try:
+        await StartAsyncTcpServer(
+            context,
+            identity=identity,
+            address=("0.0.0.0", 5020),
+            framer="rtu"
+        )
+    except Exception as e:
+        _LOGGER.error("Failed to start Modbus server: %s", str(e))
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the SDM630 simulated sensor."""
     name = config.get(CONF_NAME, DEFAULT_NAME)
+    
+    # Start the Modbus server in the background
+    hass.loop.create_task(start_modbus_server())
+    
     async_add_entities([SDM630SimSensor(name)])
 
 class SDM630SimSensor(SensorEntity):
     def __init__(self, name):
+        """Initialize the sensor."""
         self._attr_name = name
-        self._attr_native_value = 1234.5
+        self._attr_native_value = None
         self._attr_native_unit_of_measurement = POWER_WATT
         self._attr_unique_id = "sdm630_simulator_power"
+        self._attr_should_poll = True  # Enable polling
+        
+    async def async_update(self):
+        """Fetch new state data for the sensor."""
+        try:
+            # Get the power value from register 12 (Total System Power)
+            self._attr_native_value = input_reg_manager.get_float(TOTAL_POWER)
+            _LOGGER.debug("Updated sensor value to: %s", self._attr_native_value)
+        except Exception as e:
+            _LOGGER.error("Error updating sensor value: %s", str(e))
+            self._attr_native_value = None
 
     @property
     def name(self):
+        """Return the name of the sensor."""
         return self._attr_name
 
     @property
     def native_value(self):
+        """Return the state of the sensor."""
         return self._attr_native_value
 
     @property
     def native_unit_of_measurement(self):
+        """Return the unit of measurement."""
         return self._attr_native_unit_of_measurement
 
     @property
     def unique_id(self):
+        """Return a unique ID to use for this entity."""
         return self._attr_unique_id
+        
+    def set_power(self, value):
+        """Set the power value in the Modbus register."""
+        try:
+            input_reg_manager.set_float(12, float(value))
+            _LOGGER.info("Set power value to: %s", value)
+        except Exception as e:
+            _LOGGER.error("Error setting power value: %s", str(e))
